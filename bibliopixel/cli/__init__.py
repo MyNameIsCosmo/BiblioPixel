e7c3a3fb023a07dc24e0422f314541f217be8346
Cosmo Borsky <me@cosmoborsky.com>
2018-05-27 00:37:32 -0400

    __command__ to command, __name__ as name

--- bibliopixel/cli/__init__.py
+++ bibliopixel/cli/__init__.py
@@ -2,7 +2,8 @@
 
 """
 
-from .__command__ import *
+from .command import *
 from .version import Version
 
-__all__ = ["Command"]
+__all__ = ["Command", "get_command",
+           "get_availble_commands", "get_available_command_names"]
--- bibliopixel/cli/version.py
+++ bibliopixel/cli/version.py
@@ -3,7 +3,7 @@
 Prints version information
 """
 
-from bibliopixel import __name__, __version__, __status__, __copyright__
+from bibliopixel import __name__ as name, __version__, __status__, __copyright__
 from bibliopixel.cli import Command
 
 
@@ -12,7 +12,7 @@ class Version(Command):
     description = "Prints the version, copyright, and release status"
 
     def main(self):
-        print(__name__ + " "
+        print(name + " "
               + __status__
               + " v" + __version__
               + "\n" + __copyright__)
